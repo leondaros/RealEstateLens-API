9e37d92768e6a67d0470eefe294f2aa74f03c04f
Leon Daros <leondaros@gmail.com>
2024-11-02 22:06:23 +0000

    Create ViewSets

--- real_estate_lens/views.py
+++ real_estate_lens/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from real_estate_lens.models import User,Property,Location
+from real_estate_lens.serializers import UserSerializer, LocationSerializer, PropertySerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = PropertySerializer
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = LocationSerializer
